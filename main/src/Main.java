e3939817b01aa4ff44d3953baef8f9bc08743d69
ArtiomC <artiom.tomas@gmail.com>
2020-10-30 12:21:21 +0200

    commit Main from Branch2 Task 7

--- main/src/Main.java
+++ main/src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First change in Branch1");
         System.out.println("Change from Branch2");
+        MethodTwo(5, 8);
+    }
+
+
+    public static int MethodTwo(int a, int b) {
+        return a + b;
     }
 }
